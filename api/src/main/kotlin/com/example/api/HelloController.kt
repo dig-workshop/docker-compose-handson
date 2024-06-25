9b36f2768fefd01e912323a85cfbbdb6aadeb54e
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2024-06-25 13:49:00 +0900

    Controllerの実装

--- api/src/main/kotlin/com/example/api/HelloController.kt
+++ api/src/main/kotlin/com/example/api/HelloController.kt
@@ -1,1 +1,19 @@
 package com.example.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class HelloController(
+    private val userRepository: UserRepository
+) {
+    @GetMapping("/hello")
+    fun getHello(
+        @RequestParam lastName: String
+    ): String {
+        return "Hello $lastName"
+    }
+}
