451f9d8a04fb0137ecaa416282769e74612cede7
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2024-07-03 19:32:39 +0900

    add: 完成系を実装

--- we-do/api/src/main/kotlin/com/example/api/HelloController.kt
+++ we-do/api/src/main/kotlin/com/example/api/HelloController.kt
@@ -12,6 +12,23 @@ class HelloController(
     fun getHello(
         @RequestParam lastName: String
     ): String {
-        return "Hello $lastName"
+        val user = userRepository.findByLastName(lastName) ?: return "User not found..."
+
+        return "Hello ${user.firstName}"
+    }
+
+    @GetMapping("/users")
+    fun getUsers(): List<UserRecord> {
+        return userRepository.findAll()
+    }
+
+    @PostMapping("user")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun saveUser(
+        @RequestParam firstName: String,
+        @RequestParam lastName: String
+    ): UserRecord {
+        val user = UserRecord(firstName = firstName, lastName = lastName)
+        return userRepository.save(user)
     }
 }
